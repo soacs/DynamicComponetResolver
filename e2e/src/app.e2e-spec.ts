c68ade21fc85651aa7656b66e2cf636817ea6850
steve@soaconsultingservices.com <steve@soaconsultingservices.com>
2018-05-21 13:10:09 -0400

    init commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,21 @@
-import { AppPage } from './app.po';
+'use strict'; // necessary for es6 output in node
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+import { browser, element, by } from 'protractor';
 
-  beforeEach(() => {
-    page = new AppPage();
+/* tslint:disable:quotemark */
+describe('Dynamic Component Loader', function () {
+
+  beforeEach(function () {
+    browser.get('');
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should load ad banner', function () {
+    let headline = element(by.xpath("//h4[text()='Featured Hero Profile']"));
+    let name = element(by.xpath("//h3[text()='Bombasto']"));
+    let bio = element(by.xpath("//p[text()='Brave as they come']"));
+
+    expect(name).toBeDefined();
+    expect(headline).toBeDefined();
+    expect(bio).toBeDefined();
   });
 });
